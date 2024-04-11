906164c13c202118f6c993452838293eb399a279
Szefooo <144339488+Szefooo@users.noreply.github.com>
2024-04-11 13:13:01 +0200

    Add files via upload

---
+++ praca2.js
@@ -0,0 +1,6 @@
+function calculate() {
+    var number1 = parseInt(document.getElementById('inputNumber1').value);
+    var number2 = parseInt(document.getElementById('inputNumber2').value);
+    var result = number1 + number2;
+    document.getElementById('result').innerText = "Wynik: " + result;
+}
